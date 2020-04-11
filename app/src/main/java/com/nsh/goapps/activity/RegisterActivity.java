caddd6d9a093c91a3422ed851e15677ea3dc1072
nsh <uxc.wilis@gmail.com>
2020-04-11 08:40:08 +0700

    Update XML

---
+++ app/src/main/java/com/nsh/goapps/activity/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.nsh.goapps.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    protected void onCreate (Bundle saveInstanceState)
+    {
+        super.onCreate(saveInstanceState);
+        //setContentView();
+    }
+}
